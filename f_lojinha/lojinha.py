b82ab9a735d94d61289849136de969dd1f5e9644
Wallace Goulart <75147050+wallacegoulart@users.noreply.github.com>
2025-02-10 22:38:18 -0300

    Add files via upload

---
+++ f_lojinha/lojinha.py
@@ -0,0 +1,19 @@
+'''════════════════════════════════════════════════════════════════════════════╗
+║ Instituição   :  Faculdade de Tecnologia de São Paulo                        ║
+║ Solucionador  :  Lucio Nunes de Lira                                         ║
+╠══════════════════════════════════════════════════════════════════════════════╣
+║ Competição    :  InterFatecs 2019 - 1ª fase                                  ║
+║ Programa      :  Problema F - Lojinha                                        ║
+║ Linguagem     :  Python 3                                                    ║
+║ Compilador    :  CPython (3.7.2)                                             ║
+║ Versão        :  A (Rev. 0)                                                  ║
+╚════════════════════════════════════════════════════════════════════════════'''
+
+nv = int(input())
+t  = 0
+for i in range(nv):
+    q, v = input().split()
+    q = int(q)
+    v = float(v)
+    t += v * q
+print("%.2f" % t)
